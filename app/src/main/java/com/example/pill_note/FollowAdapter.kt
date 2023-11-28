3039e8d97394f66cc9dcb09cdf55eb7a2e0f1e5e
naahnoey <naahyin20@sookmyung.ac.kr>
2023-11-28 09:56:31 +0900

    design: follow fragment

---
+++ app/src/main/java/com/example/pill_note/FollowAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.pill_note
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pill_note.databinding.FollowerRecyclerviewBinding
+
+class FollowViewHolder(val binding: FollowerRecyclerviewBinding): RecyclerView.ViewHolder(binding.root)
+
+class FollowAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun getItemCount(): Int = datas.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            RecyclerView.ViewHolder
+            = FollowViewHolder(FollowerRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as FollowViewHolder).binding
+        binding.followerImage.setImageResource(android.R.drawable.picture_frame)
+        binding.followerName.text = datas[position]
+    }
+
+}
--- app/src/main/java/com/example/pill_note/MainActivity.kt
+++ app/src/main/java/com/example/pill_note/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.pill_note
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.pill_note.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    class MyFragmentPagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity){
+        val fragment = FollowFragment()
+
+        override fun getItemCount(): Int = 1
+
+        override fun createFragment(position: Int): Fragment = fragment
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = MyFragmentPagerAdapter(this)
+        binding.viewpager.adapter = adapter
     }
+
 }
