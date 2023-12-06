3a887cae76997305d2c32efa795c29f667c36727
2093ab <gil2093ab@gmail.com>
2023-12-06 20:53:33 +0900

    feat: sign up with create account

--- app/src/main/java/com/example/pill_note/AuthActivity.kt
+++ app/src/main/java/com/example/pill_note/AuthActivity.kt
@@ -3,14 +3,21 @@ package com.example.pill_note
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.pill_note.databinding.ActivityAuthBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class AuthActivity : AppCompatActivity() {
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         binding.btnSignIn.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
@@ -20,6 +27,15 @@ class AuthActivity : AppCompatActivity() {
         binding.btnSignUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            Log.d("pill_note", "current user: ${currentUser.email}")
         }
     }
 
