2646462a0fa10c916322469dd0cc26218a03d557
2093ab <gil2093ab@gmail.com>
2023-12-19 04:31:43 +0900

    feat: implement pill db

---
+++ app/src/main/java/com/example/pill_note/Pill.kt
@@ -0,0 +1,7 @@
+package com.example.pill_note
+
+data class Pill(
+    val pillname: String? = null,
+    val pillcount: String? = null
+) {
+}
--- app/src/main/java/com/example/pill_note/PillAddFragment.kt
+++ app/src/main/java/com/example/pill_note/PillAddFragment.kt
@@ -1,10 +1,18 @@
 package com.example.pill_note
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.pill_note.databinding.FragmentPillAddBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.MutableData
+import com.google.firebase.database.Transaction
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +24,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [PillAddFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PillAddFragment : Fragment() {
+class PillAddFragment : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: FragmentPillAddBinding
+    private val db = Firebase.database
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +44,33 @@ class PillAddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pill_add, container, false)
+        binding = FragmentPillAddBinding.inflate(inflater, container, false)
+        binding.mediAdd.setOnClickListener(this)
+        /*
+        binding.plusButton.setOnClickListener {
+            Log.d("pill_note", "plus button clicked")
+            binding.mediDose.text = "${binding.mediDose.text.toString().toInt() + 1}"
+        }
+        binding.minusButton.setOnClickListener {
+            Log.d("pill_note", "minus button clicked")
+            if (binding.mediDose.text.toString().toInt() > 0) {
+                binding.mediDose.text = (binding.mediDose.text.toString().toInt() - 1).toString()
+            }
+        }*/
+        return binding.root
+        //return inflater.inflate(R.layout.fragment_pill_add, container, false)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.medi_add -> {
+                Log.d("pill_note", "medi add button clicked")
+                val mediName = binding.mediName.text.toString()
+                val mediDose = binding.mediDose.text.toString()
+
+                db.getReference("pill").child(mediName).setValue(Pill(mediName, mediDose))
+            }
+        }
     }
 
     companion object {
