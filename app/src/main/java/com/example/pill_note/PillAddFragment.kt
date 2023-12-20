52ae43b5d4bdebe075d887190973bd86634e864d
2093ab <gil2093ab@gmail.com>
2023-12-20 18:16:58 +0900

    feat: pill add db

--- app/src/main/java/com/example/pill_note/PillAddFragment.kt
+++ app/src/main/java/com/example/pill_note/PillAddFragment.kt
@@ -1,5 +1,6 @@
 package com.example.pill_note
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.pill_note.databinding.FragmentPillAddBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.MutableData
@@ -30,12 +33,19 @@ class PillAddFragment : Fragment(), View.OnClickListener {
     private var param2: String? = null
     private lateinit var binding: FragmentPillAddBinding
     private val db = Firebase.database
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+
+        auth = Firebase.auth
+        if (auth.currentUser == null) {
+            val intent = Intent (activity, AuthActivity::class.java)
+            startActivity (intent)
         }
     }
 
@@ -68,7 +78,7 @@ class PillAddFragment : Fragment(), View.OnClickListener {
                 val mediName = binding.mediName.text.toString()
                 val mediDose = binding.mediDose.text.toString()
 
-                db.getReference("pill").child(mediName).setValue(Pill(mediName, mediDose))
+                db.getReference("pill").child(auth.currentUser!!.uid).setValue(Pill(mediName, mediDose))
             }
         }
     }
